15b6aea921bb72ca12319491bb45f45c433f4073
황서영 <nct830@naver.com>
2025-10-30 02:39:14 +0900

    feat : mockData 화면에 렌더링하기

--- src/components/ReviewList.jsx
+++ src/components/ReviewList.jsx
@@ -1,8 +1,12 @@
+import ReviewListItem from "./ReviewListItem";
+
 function ReviewList({ items }) {
   return (
     <ul>
       {items.map((item) => (
-        <li>{item.title}</li>
+        <li key={item.id}>
+          <ReviewListItem item={item} />
+        </li>
       ))}
     </ul>
   );
---
+++ src/components/ReviewListItem.jsx
@@ -0,0 +1,19 @@
+import styles from "./ReviewListItem.module.css";
+
+function ReviewListItem({ item }) {
+  const dateString = new Date(item.create).toLocaleDateString();
+
+  return (
+    <div className={styles.item}>
+      <img className={styles.image} src={item.imgUrl} alt={item.title} />
+      <div>
+        <h1>{item.title}</h1>
+        <p>{item.rating}</p>
+        <p>{dateString}</p>
+        <p>{item.content}</p>
+      </div>
+    </div>
+  );
+}
+
+export default ReviewListItem;
