bce0158f7c0991814792035e6f1c3f49a6f8236e
황서영 <nct830@naver.com>
2025-10-30 14:46:40 +0900

    feat : 버튼 생성(추가, 수정) -> 버튼 클릭 시 나오는 모달 생성

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import { useState } from "react";
 import ReviewList from "./components/ReviewList";
 import mockData from "./mock.json";
+import Modal from "./components/Modal";
 
 function App() {
   const [items, setItem] = useState(mockData);
   const [order, setOrder] = useState("createdAt");
+  const [isCreateReviewOpen, setIsCreateReviewOpen] = useState(false);
 
   const sortedItem = items.sort((a, b) => b[order] - a[order]);
 
@@ -17,6 +19,13 @@ function App() {
       <div>
         <button onClick={() => setOrder("createdAt")}>최신순</button>
         <button onClick={() => setOrder("rating")}>평점순</button>
+        <button onClick={() => setIsCreateReviewOpen(true)}>추가하기</button>
+        <Modal
+          isOpen={isCreateReviewOpen}
+          onClose={() => setIsCreateReviewOpen(false)}
+        >
+          생성 모달
+        </Modal>
       </div>
       <ReviewList items={sortedItem} onDelete={handleDelete} />
     </>
--- src/components/ReviewListItem.jsx
+++ src/components/ReviewListItem.jsx
@@ -1,6 +1,9 @@
+import { useState } from "react";
+import Modal from "./Modal";
 import styles from "./ReviewListItem.module.css";
 
 function ReviewListItem({ item, onDelete }) {
+  const [isEditModalOpen, setIsEditModalOpen] = useState();
   const dateString = new Date(item.createdAt).toLocaleDateString();
 
   return (
@@ -11,6 +14,13 @@ function ReviewListItem({ item, onDelete }) {
         <p>{item.rating}</p>
         <p>{dateString}</p>
         <p>{item.content}</p>
+        <button onClick={() => setIsEditModalOpen(true)}>수정</button>
+        <Modal
+          isOpen={isEditModalOpen}
+          onClose={() => setIsEditModalOpen(false)}
+        >
+          수정 모달
+        </Modal>
         <button onClick={() => onDelete(item.id)}>삭제</button>
       </div>
     </div>
