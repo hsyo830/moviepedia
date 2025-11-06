58c68aeb78cc170ae943d1da7672d96e026c77e0
황서영 <nct830@naver.com>
2025-11-06 22:55:14 +0900

    feat : 이미지 파일 input 초기화하기

--- src/components/FileInput.jsx
+++ src/components/FileInput.jsx
@@ -1,13 +1,21 @@
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import placeholderImage from "../assets/placeholder.png";
 
 function FileInput({ name, initialPreview }) {
   const [file, setFile] = useState();
   const [preview, setPreview] = useState(initialPreview);
+  const inputRef = useRef();
 
   const handleChange = (e) => {
     const nextFile = e.target.files[0];
     setFile(nextFile);
+  };
+
+  const handleClear = () => {
+    setFile(null);
+    if (inputRef.current) {
+      inputRef.current.value = "";
+    }
   };
 
   useEffect(() => {
@@ -24,8 +32,13 @@ function FileInput({ name, initialPreview }) {
 
   return (
     <>
-      <input name={name} type="file" onChange={handleChange} />
+      <input name={name} type="file" onChange={handleChange} ref={inputRef} />
       <img src={preview || placeholderImage} />
+      {file && (
+        <button type="button" onClick={handleClear}>
+          X
+        </button>
+      )}
     </>
   );
 }
